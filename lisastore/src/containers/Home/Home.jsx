b56b1592c347e3fd61933fdfe8f0f8f21dae652d
namnguyenhai <nguyenhainam26n@gmail.com>
2023-06-05 16:50:34 +0700

    change content

--- lisastore/src/containers/Home/Home.jsx
+++ lisastore/src/containers/Home/Home.jsx
@@ -4,6 +4,8 @@ import Salva from "images/Content/salva.svg";
 import Golden from "images/Content/golden.svg";
 import Furni from "images/Content/furni.svg";
 import Travell from "images/Content/travel.svg";
+import { TextFormat } from "components/Text";
+import Womenwalk from "images/Content/women_walk.svg";
 const StyledHome = styled.div`
     width: 100%;
     height: 900px;
@@ -18,8 +20,35 @@ const StyledHome = styled.div`
     }
     .recomend_home{
         width: 80%;
-        height: 700px;
+        height: 1000px;
+        /* border: 1px solid; */
+        display: flex;
         background-color: black;
+        .recomend_left{
+            width: 10%;
+            height: 100%;
+            /* border: 1px solid; */
+            display: block;
+            position: relative;
+            p{
+                position: absolute;
+                transform: rotate(-90deg);
+                bottom: 10%;
+                left: 30%;
+                transform-origin: 0 0;
+                width: 500px;
+                
+            }
+        }
+        .recomend_center{
+            width: 60%;
+            height: 100%;
+            .img{
+                height:100%;
+                width: 100%;
+                object-fit: cover;
+            }
+        }
     }
 `
 export const Home = () => {
@@ -32,8 +61,12 @@ export const Home = () => {
             <img src={Travell} alt="" />
         </div>
         <div className="recomend_home">
-            <div></div>
-            <div></div>
+            <div className="recomend_left">
+                <TextFormat size={"34px"} fontfam={"Roboto"} color={"#000000"} weight={400}>Explore new and popular styles</TextFormat>
+            </div>
+            <div className="recommend_center">
+                <img src={Womenwalk} className="img" alt="" />
+            </div>
             <div></div>
         </div>
     </StyledHome>
