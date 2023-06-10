9ca07990246ba16103e769de0de69d6246f65539
namnguyenhai <nguyenhainam26n@gmail.com>
2023-06-10 16:40:12 +0700

    change route path and readme file

--- lisastore/src/App.js
+++ lisastore/src/App.js
@@ -8,7 +8,8 @@ function App() {
     <BrowserRouter >
       <PrimaryLayout>
         <Routes>
-          <Route path='/nguyenhainam_store' element={<Home></Home>}></Route>
+          <Route path='*' element={<Home></Home>}></Route>
+
         </Routes>
       </PrimaryLayout>
     </BrowserRouter>
